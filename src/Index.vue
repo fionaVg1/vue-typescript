a670ce46fdf86e06b98bc00193a44f20bbd23f27
fiona <1125575769@qq.com>
2020-04-17 14:03:05 +0800

    init

---
+++ src/Index.vue
@@ -0,0 +1,21 @@
+<template>
+<Button @click="fn"></Button>
+</template>
+<script lang="ts">
+    import Button from '@/components/Button.vue';
+    import {
+        Component,
+        Vue
+    } from 'vue-property-decorator';
+    //Button注册到这个组件上面来
+    @Component({
+        components: {
+            Button
+        }
+    })
+    export default class Index extends Vue {
+        private fn(event: MouseEvent) {
+            console.log(1)
+        }
+    }
+</script>
---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button @click="clickEvent">
+        按钮
+    </button>
+</template>
+<script lang="ts">
+    import {
+        Vue,
+        Component,
+        Prop,
+        Emit
+    } from 'vue-property-decorator';
+    @Component
+    export default class Button extends Vue {
+        @Prop(String) private type!: string
+        @Prop(Boolean) private primary: boolean
+
+        @Emit('click')
+        private emitClick(event: MouseEvent) {
+
+        }
+        private clickEvent(event: MouseEvent) {
+            this.emitClick(event);
+        }
+        private created() {
+            console.log(this.type, this.primary);
+        }
+    }
+</script>
